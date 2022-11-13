a471ad8b2b4ea8a1e10cc8035deefe8d04dfdcdd
Alexandr Timokhin <timohinan79@yandex.ru>
2022-11-13 17:01:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 450;
+        int oneBonusMile = 20;
+        int bonusMile = (ticketPrice / oneBonusMile);
+        System.out.println("Бонусные мили:" + bonusMile);
+    }
+}
